85dda70f57bc70992ac41e78b7ae34814b9134d6
nika1337 <nikanadaraia1337@gmail.com>
2025-06-01 23:33:50 +0400

    added implementation for listening, without doing anything, just writing to console that client was accepted

--- WebServer/Program.cs
+++ WebServer/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using System.Net;
+using System.Net.Sockets;
+
+var portNumber = 8080;
+var listener = new TcpListener(IPAddress.Any, portNumber);
+listener.Start();
+Console.WriteLine($"Listening for request started on port {portNumber}");
+
+while (true)
+{
+    var client = await listener.AcceptTcpClientAsync();
+    Console.WriteLine("Accepted new client");
+    client.Close();
+}
